aa3b943a5d6f49bdbd0346553b44b8eaccb1f515
avocadoclouds <rehana447@gmail.com>
2022-01-27 11:06:57 -0800

    use class

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for User class."""
+
+from base_model import BaseModel
+
+
+class User(BaseModel):
+    """Class representing a User."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
