d96e769b8d91be6fe7e8a677b8c76fefdd9772d0
avocadoclouds <rehana447@gmail.com>
2022-01-27 11:25:51 -0800

    update methods of class FileStorage"

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,20 +5,34 @@
 class FileStorage:
 
     def __init__(self):
-        self.__file_path = 22
-        self.__objects = 11
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
         """Returns __objects dictionary."""
-        pass
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
-        pass
+        dkey = "{}.{}".format(type(obj).__name__, obj.id)
+	FileStorage.__objects[dkey] = obj.to_dict()
 
     def save(self):
         """Serialzes __objects to JSON file."""
-        pass
+        serial_objs = json.dumps(FileStorage.__objects)
+
+        if os.path.isfile(FileStorage.__file_path):
+            if os.path.getsize(FileStorage.__file_path) == 0:
+                obj_to_save = serial_objs
+
+            else:
+                obj_to_save = '\n' + serial_objs
+
+        else:
+		obj_to_save = serial_objs
+
+        with open(FileStorage.__file_path, 'a') as f:
+	    f.write(obj_to_save)
 
     def reload(self):
         """Deserializes JSON file into __objects."""
