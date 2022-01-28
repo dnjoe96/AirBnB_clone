4d3296994671fdc7fa0d1bb0a433888736dce7dd
dnjoe96 <donjoedbest@gmail.com>
2022-01-28 10:54:26 +0100

    Fix Import Error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module for FileStorage class."""
 
-import json
+import json, os
 
 class FileStorage:
-    self.__file_path = "file.json"
-    self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def __init__(self):
         pass
@@ -17,28 +17,28 @@ class FileStorage:
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
         dkey = "{}.{}".format(type(obj).__name__, obj.id)
-	    FileStorage.__objects[dkey] = obj.to_dict()
+        FileStorage.__objects[dkey] = obj.to_dict()
 
     def del_obj(self, object_id):
-		FileStorage.__objects.pop(object_id)
+        FileStorage.__objects.pop(object_id)
 
     def save(self):
         """Serialzes __objects to JSON file."""
         if len(FileStorage.__objects) != 0:
-			serial_objs = json.dumps(FileStorage.__objects)
+            serial_objs = json.dumps(FileStorage.__objects)
 
-			with open(FileStorage.__file_path, 'w') as f:
-				f.write(serial_objs)
+            with open(FileStorage.__file_path, 'w') as f:
+                f.write(serial_objs)
 
     def reload(self):
         """Deserializes JSON file into __objects."""
         FileStorage.__objects.clear()
-		
-		if not os.path.isfile(FileStorage.__file_path):
-			return
 
-		if os.path.getsize(FileStorage.__file_path) == 0:
-			return
+        if not os.path.isfile(FileStorage.__file_path):
+            return
 
-		with open(FileStorage.__file_path, "r") as f:
-			FileStorage.__objects = json.loads(f.read())
+        if os.path.getsize(FileStorage.__file_path) == 0:
+            return
+
+        with open(FileStorage.__file_path, "r") as f:
+            FileStorage.__objects = json.loads(f.read())
